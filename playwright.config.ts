ff6eeb70a71fe5bb6e1c8928f0b2fed3981fc9d7
aggnikhil94 <48529943+aggnikhil94@users.noreply.github.com>
2025-08-29 08:32:32 +0530

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout:  30*1000,      
+  expect: { timeout: 5000 }, 
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    headless: true,                  // run in headless mode
+    baseURL: 'https://playwright.dev/',  // default base url
+    trace: 'on-first-retry',         // collect trace only on retry
+    screenshot: 'on',   // capture screenshots on failure
+    video: 'retain-on-failure',      // keep video only if failed
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 0,                // timeout for actions (0 = disabled)
+    navigationTimeout: 30000,        // navigation timeout
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome']    , viewport: { width: 1280, height: 720 }},
+    },
+  ],
+});
